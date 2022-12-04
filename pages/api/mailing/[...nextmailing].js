60fd10d63205ca0435f6d991429c89647269b871
Umar Ahmed <umar.ahmed1998@gmail.com>
2022-12-04 15:42:44 -0500

    add to API route

--- pages/api/mailing/[...nextmailing].js
+++ pages/api/mailing/[...nextmailing].js
@@ -3,3 +3,7 @@ import NextMailing from "next-mailing/api";
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default NextMailing();
+
+export const config = {
+  unstable_includeFiles: ["emails/**/*.{js,ts,jsx,tsx}"],
+};
