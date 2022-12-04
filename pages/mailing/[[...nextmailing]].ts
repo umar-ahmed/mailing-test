9c28c251a75fc27b1181b20d596f66acbec405d4
Umar Ahmed <umar.ahmed1998@gmail.com>
2022-12-04 15:32:43 -0500

    try unstable include files

--- pages/mailing/[[...nextmailing]].ts
+++ pages/mailing/[[...nextmailing]].ts
@@ -4,3 +4,7 @@ import { getServerSideProps } from "next-mailing/page/server";
 export default NextMailingPage;
 
 export { getServerSideProps };
+
+export const config = {
+  unstable_includeFiles: ["emails/**/*.{js,ts,jsx,tsx}"],
+};
